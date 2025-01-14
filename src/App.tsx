1166550ad77fd72f6671b1704fb5ee8f61c8b18f
Yulian Khai <yulik19971997@gmail.com>
2025-01-14 19:41:52 +0200

    1. add components: navbar, footer. 2. pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router"
+import { Navbar } from "./components/layout/Navbar"
+import { Footer } from "./components/layout/Footer"
+import { Home as Homepage} from "./pages/Home";
+import { About as AboutPage } from "./pages/About";
+import { NotFound as NotFoundPage } from "./pages/NotFound";
 
+function App() {
 	return (
-		<>
-			<h1 className="text-3xl font-bold underline">
-				Hello world!
-			</h1>
-			<button className="btn">Click</button>
-		</>
+		<BrowserRouter>
+			<div className="flex flex-col justify-between h-screen">
+				<Navbar />
+				<main className="container mx-auto px-3 pb-12">
+					<Routes>
+						<Route path="/" element={<Homepage />} />
+						<Route path="/about" element={<AboutPage />} />
+						<Route path="/*" element={<NotFoundPage />} />
+					</Routes>
+				</main>
+				<Footer />
+			</div>
+		</BrowserRouter>
 	)
 }
 
---
+++ src/pages/About.tsx
@@ -0,0 +1,10 @@
+export const About = () => {
+	return (
+		<>
+			<h1 className='text-6xl mb-4'>Github Finder</h1>
+			<p className='text-lg text-gray-400'>
+				Version <span className='text-white'>1.0.0</span>
+			</p>
+		</>
+	)
+}
